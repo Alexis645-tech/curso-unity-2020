240f9b95f04387d21b5a2c2fea773e3e4dd2d73e
Alexis645-tech <castroalexis645@gmail.com>
2021-03-05 16:15:08 -0500

    proyecto 5

---
+++ 05_quickClick/Assets/scripts/Target.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    private float minForce = 12, maxForce = 20, maxTorque = 10, xRange = 4, ySpawnPos = -6; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Genera un vector aleatorio en tres dimensiones
+    /// </summary>
+    /// <returns>Fuerza aleatoria hacia arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+    /// <summary>
+    /// Genera un número aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    /// <summary>
+    /// Genera una posición aleatoria
+    /// </summary>
+    /// <returns>Posición aleatoria en 3D, con la coordenada z = 0</returns>
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
