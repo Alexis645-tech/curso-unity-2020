2f8638b00b0a3483bb7821c46f1e1fa022c02507
Alexis645-tech <castroalexis645@gmail.com>
2021-03-09 17:42:33 -0500

    Proyecto Final

---
+++ John Lemmon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 moveMent;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+
+    [SerializeField]
+    private float turnSpeed;
+
+    private Quaternion rotation =  Quaternion.identity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        moveMent.Set(horizontal, 0, vertical);
+        moveMent.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        _animator.SetBool("isWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, moveMent, 
+            turnSpeed * Time.deltaTime, 0f);
+
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + moveMent * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
